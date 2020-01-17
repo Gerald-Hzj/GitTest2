0a1dc7f1b8193ab384922f75437f0c14f57951b0
Zhang Zhaoxiong <zhaoxiongzhang@icloud.com>
2020-01-17 10:42:12 +0800

    为学生管理系统的学院信息创建实体类

---
+++ src/main/java/com/offcn/model/Department.java
@@ -0,0 +1,49 @@
+package com.offcn.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ * @author  张招雄
+ */
+
+@Entity
+public class Department {
+    @Id
+    private String dep_id;
+    private String dep_name;
+    private String master_name;
+    private String slogan;
+
+    public String getDep_id() {
+        return dep_id;
+    }
+
+    public void setDep_id(String dep_id) {
+        this.dep_id = dep_id;
+    }
+
+    public String getDep_name() {
+        return dep_name;
+    }
+
+    public void setDep_name(String dep_name) {
+        this.dep_name = dep_name;
+    }
+
+    public String getMaster_name() {
+        return master_name;
+    }
+
+    public void setMaster_name(String master_name) {
+        this.master_name = master_name;
+    }
+
+    public String getSlogan() {
+        return slogan;
+    }
+
+    public void setSlogan(String slogan) {
+        this.slogan = slogan;
+    }
+}
